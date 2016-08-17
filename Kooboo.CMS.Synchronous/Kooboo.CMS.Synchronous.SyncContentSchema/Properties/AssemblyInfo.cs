d2bbc4202a01b46512a385015fde93df5142cef4
Tsingbo-Kooboo <qingbo@kooboo.com>
2016-08-17 11:05:05 +0800

    Kooboo.CMS.Synchronous

--- Kooboo.CMS.Synchronous/Kooboo.CMS.Synchronous.SyncContentSchema/Properties/AssemblyInfo.cs
+++ Kooboo.CMS.Synchronous/Kooboo.CMS.Synchronous.SyncContentSchema/Properties/AssemblyInfo.cs
@@ -8,11 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Kooboo.CMS.Synchronous.SyncContentSchema")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Kooboo.CMS.Synchronous.SyncContentSchema")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b579b53c-48e0-4cd9-8135-e76772f64a12")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
